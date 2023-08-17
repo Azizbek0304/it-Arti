dbdbaaf94e20af412d6bc81e71290b7d35ccb162
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-17 10:43:52 +0500

    2 files modified

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.css';
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">
         <img src="logo.png" alt="Logo" />
       </div>
-      <div className="nav-items">
-        <div className="nav-links">
-          <ul className="nav-list">
+      <div className={`nav-items ${menuOpen ? 'open' : ''}`}>
+        <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+          <ul className={`nav-list ${menuOpen ? 'open' : ''}`}>
             <li className="nav-item">
               <a href="/">Home</a>
             </li>
@@ -17,6 +23,9 @@ const Navbar = () => {
               <a href="/about">About Us</a>
             </li>
           </ul>
+        </div>
+        <div className="menu-toggle" onClick={handleMenuToggle}>
+          <i className="fas fa-bars"></i>
         </div>
         <div className="basket-icon">
           <a href="/basket">
