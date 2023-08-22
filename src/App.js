d0135483332135eb2ac2abd31382e4af4def8a83
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-22 11:42:13 +0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from "react";
-import Header from './components/Header'
-import "./style.css";
+import React from 'react';
+import Header from './components/Header';
+import './style.css';
 
 export default function App() {
   return (
     <>
       <Header />
-
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
     </>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,8 @@
 import React, { useState } from 'react';
-import { faBars, faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { logo } from '../../assets/logo.svg';
 import './header.css';
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleMenuToggle = () => {
-    setMenuOpen((prevState) => !prevState);
-  };
-
-  const navLinks = [
-    { title: 'Home', path: '/' },
-    { title: 'About Us', path: '/about' },
-    // Add more navigation items as needed
-  ];
-
   return (
     <nav className="navbar">
       <div className="navbar-logo">
