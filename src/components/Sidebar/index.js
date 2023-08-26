153875f6eca041a852301f4abd905e15f25c6ff9
Azizbek0304 <115786719+Azizbek0304@users.noreply.github.com>
2023-08-26 18:28:15 +0500

    2 files modified

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,47 +1,85 @@
-import React, { useState } from 'react';
-import { FaHome, FaUser, FaCog, FaChartBar } from 'react-icons/fa';
-import './sidebar.css';
+import React, { useEffect, useState } from 'react';
+import './sidebar.css'; // Make sure to import your CSS file
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBars,
+  faHome,
+  faAddressBook,
+  faTachometerAlt,
+  faEnvelope,
+  faMap,
+  faCog,
+  faAngleRight,
+  faCaretDown,
+} from '@fortawesome/free-solid-svg-icons'; // Import FontAwesome icons
 
 const Sidebar = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
 
   const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+    setIsSidebarCollapsed(!isSidebarCollapsed);
   };
 
+  const sidebarItems = [
+    { icon: <FontAwesomeIcon icon={faHome} />, text: 'Home' },
+    { icon: <FontAwesomeIcon icon={faAddressBook} />, text: 'Contact' },
+    {
+      icon: <FontAwesomeIcon icon={faTachometerAlt} />,
+      text: 'General',
+      subItems: [
+        { icon: null, text: 'Dashboard' },
+        { icon: null, text: 'Analytics' },
+      ],
+    },
+    { icon: <FontAwesomeIcon icon={faEnvelope} />, text: 'Email' },
+    { icon: <FontAwesomeIcon icon={faMap} />, text: 'Map' },
+    {
+      icon: <FontAwesomeIcon icon={faCog} />,
+      text: 'Settings',
+      subItems: [
+        { icon: null, text: 'Profile' },
+        { icon: null, text: 'Preferences' },
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    // Add event listeners or any other initialization code here
+  }, []);
+
   return (
-    <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
-      <div className="sidebar-header">
-        <h1>My App</h1>
+    <div className={`sidebar ${isSidebarCollapsed ? 'collapsed' : ''}`}>
+      <div className="toggle-button" onClick={toggleSidebar}>
+        <FontAwesomeIcon icon={faBars} />
       </div>
-      <button className="sidebar-toggle-button" onClick={toggleSidebar}>
-        <div className={`toggle-icon ${isSidebarOpen ? 'open' : ''}`} />
-      </button>
-      <ul className="sidebar-menu">
-        <li className="menu-item">
-          <a href="/">
-            <FaHome className="menu-icon" />
-            Home
-          </a>
-        </li>
-        <li className="menu-item">
-          <a href="/profile">
-            <FaUser className="menu-icon" />
-            Profile
-          </a>
-        </li>
-        <li className="menu-item">
-          <a href="/analytics">
-            <FaChartBar className="menu-icon" />
-            Analytics
-          </a>
-        </li>
-        <li className="menu-item">
-          <a href="/settings">
-            <FaCog className="menu-icon" />
-            Settings
-          </a>
-        </li>
+      <ul className="sidebar-list">
+        {sidebarItems.map((item, index) => (
+          <li key={index} className="sidebar-item">
+            <div className="sidebar-icon">{item.icon}</div>
+            <span
+              className={`sidebar-text ${
+                isSidebarCollapsed ? 'collapsed' : ''
+              }`}
+            >
+              {item.text}
+            </span>
+            {item.subItems && (
+              <>
+                <FontAwesomeIcon
+                  icon={isSidebarCollapsed ? faCaretDown : faAngleRight}
+                  className="sub-menu-icon"
+                />
+                <ul className="sub-list">
+                  {item.subItems.map((subItem, subIndex) => (
+                    <li key={subIndex} className="sub-list-item">
+                      <FontAwesomeIcon icon={faAngleRight} /> {subItem.text}
+                    </li>
+                  ))}
+                </ul>
+              </>
+            )}
+          </li>
+        ))}
       </ul>
     </div>
   );
